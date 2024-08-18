ed6004bcc11c661985a660b6dcf11f7c6b155212
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-18 05:58:51 +0530

    Update slug/page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import { CustomMDX } from "app/components/mdx";
-import { formatDate, getBlogPosts } from "app/blog/utils";
+import { formatDate, getBlogPosts } from "app/lib/posts";
 import { baseUrl } from "app/sitemap";
 
 export async function generateStaticParams() {
