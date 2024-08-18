522c444ff8f3cc5ae5d3bb73f815672877be872f
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-18 05:58:02 +0530

    Update components/posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { formatDate, getBlogPosts } from "app/blog/utils";
+import { formatDate, getBlogPosts } from "app/lib/posts";
 
 export function BlogPosts() {
   let allBlogs = getBlogPosts();
