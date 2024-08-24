df6748cd2f5ea2d0d8cd31bdfafd733bfc9ad3ec
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-24 16:36:25 +0530

    Add projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,24 @@
+export default function Projects() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">Projects</h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          <a href="#">Project One</a>: Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Laboriosam veritatis rem dolorem praesentium dicta
+          labore, at laudantium quisquam.
+        </p>
+        <p>
+          <a href="#">Project Two</a>: Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Perspiciatis vero beatae, libero quibusdam sunt
+          repellat qui.
+        </p>
+        <p>
+          <a href="#">Project Three</a>: Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Quam facere dolorem repudiandae laboriosam adipisci
+          sit ipsam corporis.
+        </p>
+      </div>
+    </section>
+  );
+}
