004138857824fd6594eea40a4eabe4768efa89ba
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-28 04:53:52 +0530

    Add post component code

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,45 @@
-import { BlogPosts } from "app/components/posts";
+import Link from "next/link";
+import { formatDate, getBlogPosts } from "app/lib/posts";
 
 export const metadata = {
   title: "Blog",
   description: "Nextfolio Blog",
 };
 
-export default function Page() {
+export default function BlogPosts() {
+  let allBlogs = getBlogPosts();
+
   return (
     <section>
       <h1 className="mb-8 text-2xl font-medium tracking-tighter">Our Blog</h1>
-      <BlogPosts />
+      <div>
+        {allBlogs
+          .sort((a, b) => {
+            if (
+              new Date(a.metadata.publishedAt) >
+              new Date(b.metadata.publishedAt)
+            ) {
+              return -1;
+            }
+            return 1;
+          })
+          .map((post) => (
+            <Link
+              key={post.slug}
+              className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80"
+              href={`/blog/${post.slug}`}
+            >
+              <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
+                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                  {post.metadata.title}
+                </p>
+                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                  {formatDate(post.metadata.publishedAt, false)}
+                </p>
+              </div>
+            </Link>
+          ))}
+      </div>
     </section>
   );
 }
