4b802d16c1d7f7f97e45394b96898bdda9ca605a
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-28 17:41:28 +0530

    Add project-data.tsx

---
+++ app/projects/project-data.tsx
@@ -0,0 +1,30 @@
+export interface Project {
+  title: string;
+  year: number;
+  description: string;
+  url: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: "Project One",
+    year: 2023,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+    url: "https://example.com/project-alpha",
+  },
+  {
+    title: "Project Two",
+    year: 2022,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+    url: "https://example.com/beta-initiative",
+  },
+  {
+    title: "Project Three",
+    year: 2021,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+    url: "https://example.com/gamma-solution",
+  },
+];
