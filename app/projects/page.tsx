00f76fb6b8fb3735be111f03ff6be5882ba87322
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-28 17:42:16 +0530

    Update projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,23 +1,34 @@
+import React from "react";
+import { projects } from "./project-data";
+
 export default function Projects() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-medium tracking-tighter">Projects</h1>
-      <div className="prose prose-neutral dark:prose-invert">
-        <p>
-          <a href="#">Project One</a>: Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Laboriosam veritatis rem dolorem praesentium dicta
-          labore, at laudantium quisquam.
-        </p>
-        <p>
-          <a href="#">Project Two</a>: Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Perspiciatis vero beatae, libero quibusdam sunt
-          repellat qui.
-        </p>
-        <p>
-          <a href="#">Project Three</a>: Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Quam facere dolorem repudiandae laboriosam adipisci
-          sit ipsam corporis.
-        </p>
+      <div className="space-y-6">
+        {projects.map((project, index) => (
+          <a
+            key={index}
+            href={project.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block group hover:opacity-80 transition-opacity duration-200"
+          >
+            <div className="flex flex-col">
+              <div className="w-full flex justify-between items-baseline">
+                <span className="text-black dark:text-white font-medium tracking-tight">
+                  {project.title}
+                </span>
+                <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                  {project.year}
+                </span>
+              </div>
+              <p className="prose prose-neutral dark:prose-invert pt-3">
+                {project.description}
+              </p>
+            </div>
+          </a>
+        ))}
       </div>
     </section>
   );
