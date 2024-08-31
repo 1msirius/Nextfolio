fded443c6eb87cfc180969a90bbe648479aed13d
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-31 23:57:08 +0530

    Add RSS, Atom, and JSON feed links to <head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cx(GeistSans.variable, GeistMono.variable)}>
+      <head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          href="/rss.xml"
+          title="RSS Feed"
+        />
+        <link
+          rel="alternate"
+          type="application/atom+xml"
+          href="/atom.xml"
+          title="Atom Feed"
+        />
+        <link
+          rel="alternate"
+          type="application/feed+json"
+          href="/feed.json"
+          title="JSON Feed"
+        />
+      </head>
       <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
         <ThemeProvider
           attribute="class"
