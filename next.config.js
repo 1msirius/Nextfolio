9a62d47ef942e71bf38fcf3d6267b7e9ae793967
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-08-31 21:45:59 +0530

    Add Rewrites for Feed Endpoints

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/feed.xml",
+        destination: "/feed/feed.xml",
+      },
+      {
+        source: "/atom.xml",
+        destination: "/feed/atom.xml",
+      },
+      {
+        source: "/feed.json",
+        destination: "/feed/feed.json",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
