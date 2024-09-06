9a5978d733e214be659078cc3146f27936ed686b
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-06 06:49:32 +0530

    Add Twitter metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,10 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  twitter: {
+    title: metaData.name,
+    card: "summary_large_image",
+  },
   icons: {
     icon: "/favicon.ico",
   },
