b7fd64c658fc00fafc0557d3eaec0f854fe1a241
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-15 20:57:07 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         />
       </a>
 
-      <h1 className="mb-8 text-2xl font-medium tracking-tighter">
+      <h1 className="mb-8 text-2xl font-medium tracking-tight">
         Portfolio, made simple!
       </h1>
 
