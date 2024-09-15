ba5d2f6a5590fb16b9d9d7cdf0f2f25564acc613
Sirius <118716346+1msirius@users.noreply.github.com>
2024-09-16 04:12:54 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+      <h1 className="font-medium text-2xl mb-8 tracking-tight">
         404 - Page not found
       </h1>
       <p className="mb-4">Oops! The page you're looking for doesn't seem to exist.</p>
