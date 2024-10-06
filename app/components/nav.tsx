9ea2d7385e478d02cbec2d5972298e1d9b3f824c
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-10-07 02:18:18 +0530

    Update nav title size and padding

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -10,10 +10,10 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <nav className="lg:mb-16 mb-12 py-4">
+    <nav className="lg:mb-16 mb-12 py-5">
       <div className="flex flex-col md:flex-row md:items-center justify-between">
         <div className="flex items-center">
-          <Link href="/" className="text-[28px] font-semibold tracking-tight">
+          <Link href="/" className="text-3xl font-semibold tracking-tight">
             {metaData.title}
           </Link>
         </div>
