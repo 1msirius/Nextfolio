55a12ae2511fd49412d572f555577239c5d49519
Melle <melle@git.com>
2024-12-30 18:54:30 +0100

    Use next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   async rewrites() {
@@ -36,4 +38,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
