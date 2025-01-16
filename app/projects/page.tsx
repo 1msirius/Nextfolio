ca7cde7557053d9e5ebb7c3be78360b191c9a642
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-01-17 00:37:52 +0530

    Update project page theme

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,39 +1,32 @@
-import React from "react";
+import Link from "next/link";
 import type { Metadata } from "next";
 import { projects } from "./project-data";
 
 export const metadata: Metadata = {
   title: "Projects",
-  description: "My Projects",
+  description: "Nextfolio Projects",
 };
 
 export default function Projects() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-medium tracking-tight">Projects</h1>
-      <div className="space-y-6">
+      <div>
         {projects.map((project, index) => (
-          <a
+          <Link
             key={index}
             href={project.url}
+            className="flex flex-col space-y-1 mb-5 transition-opacity duration-200 hover:opacity-80"
             target="_blank"
             rel="noopener noreferrer"
-            className="block group hover:opacity-80 transition-opacity duration-200"
           >
-            <div className="flex flex-col">
-              <div className="w-full flex justify-between items-baseline">
-                <span className="text-black dark:text-white font-medium tracking-tight">
-                  {project.title}
-                </span>
-                <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                  {project.year}
-                </span>
-              </div>
-              <p className="prose prose-neutral dark:prose-invert pt-3">
+            <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
+              <h2 className="text-black dark:text-white">{project.title}</h2>
+              <p className="text-neutral-600 dark:text-neutral-400 tracking-tight">
                 {project.description}
               </p>
             </div>
-          </a>
+          </Link>
         ))}
       </div>
     </section>
