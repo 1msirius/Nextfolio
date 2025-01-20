125a32432bc8493011985781088a32fe479859b6
Suman Paik <reachout.paik@gmail.com>
2025-01-20 15:58:36 +0530

    Update layout.tsx
    IN THE UPDATED THEME-SWITCH.TSX WE ARE NOT USING THE PROPS

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,10 +76,10 @@ export default function RootLayout({
       </head>
       <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
         <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+{/*           attribute="class" */}
+{/*           defaultTheme="system" */}
+{/*           enableSystem */}
+{/*           disableTransitionOnChange */}
         >
           <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[640px] w-full">
             <Navbar />
