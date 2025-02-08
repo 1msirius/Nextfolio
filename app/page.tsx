e99a30890ab5feb796efbe9fecb4af86cef83046
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-02-08 23:25:05 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         />
       </a>
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Portfolio, made simple!
+        Portfolio template!
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
