f2ccd064a65961616a3e4bc8ff8e6c90a8bb3eee
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-06-11 00:20:53 +0530

    Update config path

---
+++ app/lib/config.ts
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "https://nextfolio-template.vercel.app/",
+  title: "Nextfolio",
+  name: "Sirius",
+  ogImage: "/opengraph-image.png",
+  description:
+    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+};
+
+export const socialLinks = {
+  twitter: "https://x.com/1tssirius",
+  github: "https://github.com/1msirius/Nextfolio",
+  instagram: "https://www.instagram.com/",
+  linkedin: "https://www.linkedin.com/",
+  email: "mailto:example@gmail.com",
+};
